d9d18983abef09d688c0171e72a48f3a8d8fc757
zkamiyama <68386470+zkamiyama@users.noreply.github.com>
2026-09-02 16:36:49 +0900

    Generate requested native mode in stamp fixture

--- examples/source_stamp_fixture.rs
+++ examples/source_stamp_fixture.rs
@@ -5,11 +5,22 @@ use std::env;
 use std::fs;
 use std::path::PathBuf;
 
+fn parse_mode(value: &str) -> Result<ReaperPeakMode, Box<dyn std::error::Error>> {
+    match value {
+        "waveform" => Ok(ReaperPeakMode::Waveform),
+        "spectral" => Ok(ReaperPeakMode::Spectral),
+        "spectrogram" => Ok(ReaperPeakMode::Spectrogram),
+        _ => Err(format!("unknown REAPER peak mode: {value}").into()),
+    }
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut args = env::args_os().skip(1);
     let source = PathBuf::from(args.next().ok_or("missing source path")?);
     let pcm_path = PathBuf::from(args.next().ok_or("missing PCM16LE path")?);
     let output = PathBuf::from(args.next().ok_or("missing output path")?);
+    let mode_arg = args.next().ok_or("missing REAPER peak mode")?;
+    let mode = parse_mode(&mode_arg.to_string_lossy())?;
     if args.next().is_some() {
         return Err("unexpected extra arguments".into());
     }
@@ -33,7 +44,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         spectral: false,
     }
     .with_source_stamp(stamp);
-    let blob = generate_pcm16_reaper(&pcm, &options, ReaperPeakMode::Waveform)?;
+    let blob = generate_pcm16_reaper(&pcm, &options, mode)?;
     fs::write(output, blob)?;
     Ok(())
 }
