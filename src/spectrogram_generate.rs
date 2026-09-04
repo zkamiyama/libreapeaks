e990c2b477665e45b459e626efda5a9c257b79a8
zkamiyama <68386470+zkamiyama@users.noreply.github.com>
2026-09-04 22:04:21 +0900

    Fix cfg-specific FFT Clippy call

--- src/spectrogram_generate.rs
+++ src/spectrogram_generate.rs
@@ -269,7 +269,10 @@ fn analyze_base_frame(
         }
     }
 
+    #[cfg(feature = "strict-wdl")]
     let spectrum = real_fft_256(&mut input);
+    #[cfg(not(feature = "strict-wdl"))]
+    let spectrum = real_fft_256(&input);
     let mut bins = [0u16; SPECTROGRAM_BINS];
     for stored_bin in 0..SPECTROGRAM_BINS {
         let value = spectrum[stored_bin + 1];
